978970ecb471edb72e774a2921c845230d8f1910
Egor Marin <marin@phystech.edu>
2021-04-01 15:59:57 +0300

    Correct sha256 checksum check

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
+.PHONY: download clean
+
 download: download.sh
 	bash download.sh
 
-
+clean:
+	rm -r ./ftp.ebi.ac.uk
--- download.sh
+++ download.sh
@@ -9,6 +9,7 @@ files=(\
 "chembl_28_sqlite.tar.gz" \
 "chembl_28.sdf.gz" \
 "chembl_28_chemreps.txt.gz" \
+"chembl_28.fps.gz" 
 )
 
 checksum_file="${chembl_folder}/checksums.txt"
@@ -16,7 +17,7 @@ checksum_file="${chembl_folder}/checksums.txt"
 for file in "${files[@]}"; do
 	wget --no-check-certificate -c -x "${CHEMBL}/${file}"
 	if grep "${file}" ${checksum_file}; then
-		while [[ $(grep "${file}" ${checksum_file}) != $(sha256sum "$file") ]]; do
+		while [[ $(grep "${file}" ${checksum_file} | awk '{print $1}') != $(sha256sum "${chembl_folder}/${file}" | awk '{print $1}') ]]; do
 			wget --no-check-certificate -c -x "${CHEMBL}/${file}"
 		done
 	fi
